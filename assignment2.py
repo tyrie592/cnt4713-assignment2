5ede0be38ed24f8d5bf229819cd4b4d9243be19a
tyrie592 <46247477+tyrie592@users.noreply.github.com>
2024-01-28 19:52:12 -0500

    Update assignment2.py
    finally fixed anniversaries method

--- assignment2.py
+++ assignment2.py
@@ -10,24 +10,22 @@ class Assignment2:
         print(age)
 
     def listAnniversaries(self):
-        current_year = 2022  # Assuming today is the year 2024
-        anniversaries = []
-        for year in range(self.year, current_year + 1, 10):
-            if year % 10 == 0:
-                anniversaries.append(year % 100)
-        return anniversaries
+        lst = []
+        i = 10
 
-    def modifyYear(n):
-        num = a
-        res = ""
-        arr = str(num)
-        for i in range(n):
-            res += arr[:2]
-        num = num * n
-        lst = str(num)
-        arr1 = lst[::2]
-        res += arr1
-        return res
+        while (True):
+            if self.year + i < 2022:
+                lst.append(i)
+            else:
+                break
+            i = i + 10
+        return lst
+
+    def modifyYear(self, n):
+        year = str(self.year)
+        first_two_chars = year[:2]
+        odd_chars = year[::2]
+        return first_two_chars * n + odd_chars * n
 
     def checkGoodString(string): #@staicmethod
         if len(string) < 9:
@@ -51,32 +49,3 @@ class Assignment2:
              return True
          except:
              return False
-
-#test code below
-a = Assignment2(2000)
-ret = a.tellAge(2024)
-print(ret)
-
-a = Assignment2(2000)
-ret = a.listAnniversaries()
-print(ret)
-
-a = Assignment2(1991)
-ret = a.listAnniversaries()
-print(ret)
-
-a = 1782
-ret = Assignment2.modifyYear(3)
-print(ret)
-
-ret = Assignment2.checkGoodString("f1obar0more")
-print(ret)
-
-ret = Assignment2.checkGoodString("foobar0more")
-print(ret)
-
-retval = Assignment2.connectTcp("www.google.com", 80)
-if retval:
-    print("Connection established correctly")
-else:
-    print("Some error")
